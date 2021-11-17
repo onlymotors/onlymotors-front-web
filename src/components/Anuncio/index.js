6105cba070e5370cb52226329113b44b74c276af
Davi <davisilvaneves3@gmail.com>
2021-11-17 00:34:16 -0300

    Adicionando paginação

--- src/components/Anuncio/index.js
+++ src/components/Anuncio/index.js
@@ -1,27 +1,41 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
+import Pagination from "react-js-pagination";
 import anuncio_img from '../../assets/anuncio_foto.png';
 import api from '../../services/api';
+import '../../components/Anuncio/anuncio.css'
+
 
 export default function Anuncio() {
 
     const [anuncios, setAnuncios] = useState([]);
+    const [contadorPagina, setContadorPagina] = useState(1)
+    const [estado, setEstado] = useState(0)
+    const [total, setTotal] = useState([])
 
     useEffect(() => {
         async function loadAnuncios() {
             const res = await api.get('/anuncios')
                 .then((res) => {
-                    setAnuncios(res.data.anuncio)
+                    setTotal(res.data.anuncio.length)
+                    const slice = res.data.anuncio.slice((contadorPagina - 1) * 10, contadorPagina * 10);
+                    setAnuncios(slice)
                 })
         }
         loadAnuncios();
-    }, [])
+        window.scrollTo(0, 0)
+    }, [estado])
+
+    const trocarPagina = (e) => {
+        setContadorPagina(e)
+        setEstado(estado + 1)
+    }
 
     return (
         <div className='container-home'>
             <div className='container-title'>
                 <h2>Carros Novos e Usados</h2>
-                {anuncios.length === 0 ? <span>Nenhum anúncios encontrado</span> : <span>{anuncios.length} Anúncios Encontrados</span>}
+                {anuncios.length === 0 ? <span>Nenhum anúncios encontrado</span> : <span>{total} Anúncios Encontrados</span>}
             </div>
             {
                 anuncios.map((index) => {
@@ -41,6 +55,16 @@ export default function Anuncio() {
                     )
                 })
             }
+            <Pagination
+                activePage={contadorPagina}
+                itemsCountPerPage={10}
+                totalItemsCount={total}
+                pageRangeDisplayed={5}
+                onChange={(e) => trocarPagina(e)}
+                hideDisabled={true}
+                hideFirstLastPages={true}
+            />
         </div>
+
     )
 }
